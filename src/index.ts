f5a8d7644d0f629514101ffa19cf479b47e3548a
Andre <andre.rieck@gmx.de>
2023-12-20 19:30:49 +0100

    Day 5

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import Day2 from './day2/day2';
 import Day3 from './day3/day3';
 import Day4 from './day4/day4';
+import Day5 from './day5/day5';
 import DayProvider from './dayProvider';
 
-const days : { [day:number] : Array<() => string> } = [];
-
-[Day2,Day3,Day4].forEach(d => {
+[Day2,Day3,Day4,Day5].forEach(d => {
     console.log(`Advent of code 2023 Day ${d.day}`);
     console.log('-------------------------------------');
     console.log('(1)');
